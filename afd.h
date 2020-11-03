5718e4be47188cbf204614b06db197f57af64731
jeanpaulHT <jean.huby@utec.edu.pe>
2020-11-03 16:11:14 -0500

    question 2

--- afd.h
+++ afd.h
@@ -7,7 +7,9 @@
 
 #include <vector>
 #include <unordered_set>
+#include <iostream>
 #include <unordered_map>
+#include <set>
 
 class afd {
     using alphabet = bool;
@@ -23,7 +25,61 @@ public:
 
     afd(Q initial, std::unordered_set<Q> &finalState) : initialState_(initial), finalStates_(finalState) {};
 
-    void addTransition(Q beginState, alphabet symbol, Q endState) { states_[beginState][symbol] = endState; };
+    void addTransition(Q beginState, alphabet symbol, Q endState) { 
+        states_[beginState][symbol] = endState;
+    };
+
+
+    auto equivalentStates(){
+        std::vector<std::vector<bool>> equivalent;
+
+
+        for (int i = 0; i < states_.size(); i++){
+            std::vector<bool> temp (states_.size(), false);
+            equivalent.push_back(temp);
+        }
+
+
+
+        for(int i = 0; i < states_.size(); i++){
+            equivalent[i][i] = true;
+            for(int j = 0; j < i ; j++){
+                if(finalStates_.count(i) && !finalStates_.count(j)
+                || finalStates_.count(j) && !finalStates_.count(i)){
+                    equivalent[i][j] = true;
+                    equivalent[j][i] = true;
+                }
+
+            }
+        }
+
+        for(int i = 0; i < states_.size(); i++){
+            for(int j = 0; j < i ; j++){
+                std::vector<std::pair<int,int>> vec;
+                vec.push_back({states_[i][0], states_[j][0]});
+                vec.push_back({states_[i][1], states_[j][1]});
+
+                for(auto it: vec){
+                    if(it.first != it.second && equivalent[it.first][it.second]){
+                        equivalent[i][j] = true;
+                        equivalent[j][i] = true;
+                    }
+                }
+            }
+        }
+
+        return equivalent;
+    }
+
+
+    void printStates(){
+        for(auto trans: states_){
+            for(auto delta : trans.second){
+                std::cout<<trans.first<<" "<<delta.first<<" "<<delta.second<<"\n";
+            }
+        }
+    }
+    
 };
 
 
--- main.cpp
+++ main.cpp
@@ -5,17 +5,27 @@ int main() {
     int n, iState, nFStates;
     std::cin >> n >> iState >> nFStates;
     std::unordered_set<int> fState;
-    afd myAfd(iState, fState);
     while (nFStates--) {
         int i{};
         std::cin >> i;
         fState.insert(i);
     }
+    afd myAfd(iState, fState);
+
     int tmp = 2 * n;
     while (tmp--) {
         int p, a, q;
         std::cin >> p >> a >> q;
         myAfd.addTransition(p, a, q);
     }
+
+    auto res = myAfd.equivalentStates();
+
+    for(auto vec: res){
+        for(auto it: vec)
+            std::cout<<it<<" ";
+        std::cout<<'\n';
+    }
+
     return 0;
 }
